88c5be99ef108e4ed2fc50e33240bd66ab8daa7b
Max <max@max.com>
2024-04-24 00:14:47 -0400

    updated

--- react-todo-app/src/components/TodoApp.jsx
+++ react-todo-app/src/components/TodoApp.jsx
@@ -22,6 +22,15 @@ const TodoApp = () => {
         setTasks(tasks.filter((task) => task.id !== id));
     }
 
+    const completeTask = (id) => {
+        let updatedTodos = tasks.map(task => {
+            if (task.id === id) {
+                task.isComplete = !todo.isComplete;
+            }
+            return task;
+        });
+        setTasks(updatedTodos);
+    }
 
     return (
         <>  <div className="todo-wrapper">
@@ -33,7 +42,10 @@ const TodoApp = () => {
                 </div>
 
                 {tasks.map((task) =>
-                    <div className="task-container"><p className="task" key={task.id}>{task.value}</p><span onClick={() => deleteTask(task.id)}><FontAwesomeIcon icon={faTrash} /></span></div>
+                    <div className="task-container">
+                        <p className={task.isComplete ? 'completed' : ''} key={task.id} onClick={() => completeTask(task.id)}>{task.value}</p>
+                        <span className="icon" onClick={() => deleteTask(task.id)}><FontAwesomeIcon icon={faTrash} /></span>
+                    </div>
                 )}
 
             </div>
